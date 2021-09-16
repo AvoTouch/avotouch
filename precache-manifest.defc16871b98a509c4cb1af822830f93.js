262d0bd0005e6bf36fd0f894480c7ea0de88dc47
avo-labs <avogadrogeryson@gmail.com>
2021-09-16 05:05:28 +0300

    initial launch

---
+++ precache-manifest.defc16871b98a509c4cb1af822830f93.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78380ec82c07424d251f",
+    "url": "/css/app.ccfc4cc7.css"
+  },
+  {
+    "revision": "36ce32406e06e0b8df12",
+    "url": "/css/chunk-vendors.545bb55d.css"
+  },
+  {
+    "revision": "c033abf5eccd8f7baf4df0abfdc8ac0b",
+    "url": "/fonts/Montserrat-Light.c033abf5.woff2"
+  },
+  {
+    "revision": "d19e35006e83077ecf93550dd04fe5f8",
+    "url": "/fonts/Montserrat-Light.d19e3500.woff"
+  },
+  {
+    "revision": "01a6474fe55e7259bab3da1f3a9c23ab",
+    "url": "/fonts/ProximaNova-Bold.01a6474f.woff"
+  },
+  {
+    "revision": "fe1cec64865eec322e682b39c8cd54d5",
+    "url": "/fonts/ProximaNova-Bold.fe1cec64.woff2"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/fonts/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/fonts/lg.f4292655.ttf"
+  },
+  {
+    "revision": "3da53a4b8ea927b6e180b8328da60af5",
+    "url": "/img/Untitled-1.3da53a4b.png"
+  },
+  {
+    "revision": "aca37dd29623a8e023ca6acb77a6560d",
+    "url": "/img/aru_deli.aca37dd2.png"
+  },
+  {
+    "revision": "e73ca297964030a9d1de5904076a5dbb",
+    "url": "/img/bush.e73ca297.png"
+  },
+  {
+    "revision": "92c13817b9d8109ca7db778da4e55c6b",
+    "url": "/img/elearning.92c13817.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/img/icons2/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/img/icons2/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/img/icons2/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/img/icons2/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/img/icons2/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/img/icons2/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/img/icons2/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/img/icons2/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/img/icons2/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/img/icons2/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/img/icons2/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/img/icons2/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/img/icons2/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/img/icons2/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/img/icons2/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/img/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "1f109e3ea4b462ccddef6789f357b985",
+    "url": "/img/makisatu.1f109e3e.png"
+  },
+  {
+    "revision": "5cc052b9b1f67ee8047f1d46044dfb42",
+    "url": "/img/oca.5cc052b9.png"
+  },
+  {
+    "revision": "06d81f0c4a8c8b66e46e8217bf3c238f",
+    "url": "/img/timedesktop.06d81f0c.jpg"
+  },
+  {
+    "revision": "f76410520bbb89f533a9dc67abd4d494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78380ec82c07424d251f",
+    "url": "/js/app.0e0d39b6.js"
+  },
+  {
+    "revision": "36ce32406e06e0b8df12",
+    "url": "/js/chunk-vendors.35a5a59f.js"
+  },
+  {
+    "revision": "68ad06001231ce32c70d2c117e800c74",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "dae592015c6d919c4f29b3fd11e01c13",
+    "url": "/particles.json"
+  },
+  {
+    "revision": "7dc8b842d2190b47a5d061e21fd47643",
+    "url": "/particlesjs.json"
+  },
+  {
+    "revision": "aca37dd29623a8e023ca6acb77a6560d",
+    "url": "/projects/aru_deli.png"
+  },
+  {
+    "revision": "e73ca297964030a9d1de5904076a5dbb",
+    "url": "/projects/bush.png"
+  },
+  {
+    "revision": "92c13817b9d8109ca7db778da4e55c6b",
+    "url": "/projects/elearning.png"
+  },
+  {
+    "revision": "1f109e3ea4b462ccddef6789f357b985",
+    "url": "/projects/makisatu.png"
+  },
+  {
+    "revision": "5cc052b9b1f67ee8047f1d46044dfb42",
+    "url": "/projects/oca.png"
+  },
+  {
+    "revision": "06d81f0c4a8c8b66e46e8217bf3c238f",
+    "url": "/projects/timedesktop.jpg"
+  },
+  {
+    "revision": "231f89eeec548905800e6de9b74ffd1e",
+    "url": "/projects/timemob.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
